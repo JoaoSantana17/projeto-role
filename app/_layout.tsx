18dc731ce1a16b42379b53b255756a3f49244221
JoaoSantana17 <joao.lsantana17@gmail.com>
2025-11-05 21:05:30 -0300

    Layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,18 +1,23 @@
-import { Stack } from 'expo-router';
-import { StatusBar } from 'expo-status-bar';
-import { View } from 'react-native';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { View } from "react-native";
+
+const client = new QueryClient();
 
 export default function Layout() {
   return (
-    <View style={{ flex: 1, backgroundColor: '#210b34' }}>
-      <StatusBar style="light" />
-      <Stack
-        screenOptions={{
-          headerShown: false,
-          presentation: 'card',
-          animation: 'fade', 
-        }}
-      />
-    </View>
+    <QueryClientProvider client={client}>
+      <View style={{ flex: 1, backgroundColor: "#210b34" }}>
+        <StatusBar style="light" />
+        <Stack screenOptions = 
+        {{ 
+          headerShown: false, 
+          presentation: "card", 
+          animation: "fade" 
+          }} />
+      </View>
+    </QueryClientProvider>
   );
 }
